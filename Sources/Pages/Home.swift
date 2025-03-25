04aa4ac6cfecac2b936a98c6a49a00b9f2b7660b
Mikaela Caron <mikaelacaron@gmail.com>
2025-03-26 05:40:09 +0900

    only coming soon

--- Sources/Pages/Home.swift
+++ Sources/Pages/Home.swift
@@ -5,59 +5,41 @@ struct Home: StaticPage {
     var title = "CommunityKit 1.0"
     
     var body: some HTML {
-            let brandImage = Image("/images/horizontal-lockup@2x.png", description: "CommunityKit")
-                .frame(width: 200)
-            NavigationBar(logo: brandImage) {
-                
-            }
-            .background(.lightGray)
-        
-  
+        let brandImage = Image("/images/horizontal-lockup@2x.png", description: "CommunityKit")
+            .frame(width: 200)
+        NavigationBar(logo: brandImage) {
             
-            VStack(alignment: .center) {
-                VStack {
-                    Text("Meet")
-                        .font(.title1)
-                }
-                Image("/images/vertical-lockup@2x.png", description: "CommunityKit")
-                    .resizable()
-                    .frame(height: 400)
-                VStack {
-                    Text("coming to")
-                        .font(.title2)
-                    Text("WWDC 2025")
-                        .font(.title1)
-                        .fontWeight(.bold)
-                }
-            }
+        }
+        .background(.lightGray)
         
         
-
-      
-            
-            VStack(alignment: .leading) {
-                Image("/images/whatis.png", description: "What is CommunityKit?")
-                    .resizable()
-                    .frame(width: 900)
-                
-                Group {
-                    Text("• Gather with the community of developers and designers")
-                    Text("• Network with new friends and old")
-                    Text("• Get the cutting edge of WWDC 25")
-                    Text("• Attend events with us and other community staples")
-                    Text("• Join us for LIVE podcast recordings")
-                }
-                .font(.title3)
-                .fontWeight(.semibold)
-                .padding(.top, 60)
+        
+        VStack(alignment: .center) {
+            VStack {
+                Text("Meet")
+                    .font(.title1)
             }
-            .padding(75)
+            Image("/images/vertical-lockup@2x.png", description: "CommunityKit")
+                .resizable()
+                .frame(height: 400)
+            VStack {
+                Text("coming to")
+                    .font(.title2)
+                Text("WWDC2025")
+                    .font(.title1)
+                    .fontWeight(.bold)
+            }
+        }
+        
+        
+        
+        
         
         
         ZStack {
             Grid {
                 Image(decorative: "/images/bluebg.jpg")
-                        .resizable()
+                    .resizable()
             }
             
             Grid {
@@ -72,35 +54,9 @@ struct Home: StaticPage {
                 
                 Script(file: "https://communitykit.kit.com/28f3427844/index.js")
                     .data("uid", "28f3427844")
-         
+                
             }
             
         }
-    
-        
-        
-    
-            
-            VStack(alignment: .leading) {
-                Image("/images/broughtheader.png", description: "Brought to you by")
-                    .resizable()
-                    .frame(width: 900)
-                Group {
-                    Text("• Tamia James")
-                    Text("• Adrian Eves")
-                    Text("• Mikaela Caron")
-                    Text("• Paul Hudson")
-                    Text("• Michie Ang")
-                    Text("• Chris Wu")
-                    Text("• Frank Foster")
-                }
-                .font(.title1)
-                .fontWeight(.semibold)
-            
-                .padding(.top, 60)
-            }
-            .padding(75)
-        
-        
     }
 }
